d8fc0f35c0af2210740353788eb2c22cf9330fd7
ankiitaa04 <2005ankitasingh@gmail.com>
2025-05-30 10:32:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -102,5 +102,8 @@ def index():
 
     return render_template("index.html", error=error, menu_items=menu_items, restaurant_name=restaurant_name)
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Default to 5000 for local
+    app.run(host="0.0.0.0", port=port)
